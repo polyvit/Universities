fb39c9ce4e0eeb4b8784c2109dd948dc9eed30a8
Polina Limonova <polina98_08@mail.ru>
2023-11-01 00:26:18 +0300

    predeploy changes

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,22 +1,6 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 
-// init select
-// const select = document.querySelectorAll("select");
-// M.FormSelect.init(select);
-
-// export function getSelectInstance(elem) {
-//   return M.FormSelect.getInstance(elem);
-// }
-
-// Init datepicker
-// const datepicker = document.querySelectorAll(".datepicker");
-// M.Datepicker.init(datepicker);
-
-// export function getDatepickerInstance(elem) {
-//   return M.Datepicker.getInstance(elem);
-// }
-
 // init autocomplete
 const autocomplete = document.querySelectorAll(".autocomplete");
 M.Autocomplete.init(autocomplete, {
